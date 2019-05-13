3196c7b011c7c7cd29ea79669fe0f8bc2c41e493
Tanner Stephens <tannerrstephens@gmail.com>
2019-05-12 20:39:26 -0500

    Updated to support 1.14

--- app/shuffle/__init__.py
+++ app/shuffle/__init__.py
@@ -8,42 +8,47 @@ def make_shuffled_datapack():
   items = []
 
   for file in glob.glob(dir_path + '/original_recipes/*'):
-    name = file.split('/')[-1][:-5]
+    try:
+      name = file.split('/')[-1][:-5]
 
-    if name in ['crafting_table', 'furnace']:
-      continue
-    
-    if 'planks' in name:
-      continue
+      if name in ['crafting_table', 'furnace']:
+        continue
+      
+      if 'planks' in name:
+        continue
 
-    with open(file) as f:
-      data = json.load(f)
+      with open(file) as f:
+        data = json.load(f)
 
-    if 'result' not in data:
-      continue
+      if 'result' not in data:
+        continue
 
-    if data['type'] == 'crafting_shaped':
-      recipe = dict(type=data['type'], pattern=data['pattern'], key=data['key'])
-    elif data['type'] == 'crafting_shapeless':
-      recipe = dict(type=data['type'], ingredients=data['ingredients'])
-    else:
-      recipe = dict(type=data['type'], ingredient=data['ingredient'], cookingtime=data['cookingtime'], experience=data['experience'])
+      if data['type'] == 'minecraft:crafting_shaped':
+        recipe = dict(type=data['type'], pattern=data['pattern'], key=data['key'])
+      elif data['type'] == 'minecraft:crafting_shapeless':
+        recipe = dict(type=data['type'], ingredients=data['ingredients'])
+      elif data['type'] == 'minecraft:stonecutting':
+        continue
+      else:
+        recipe = dict(type=data['type'], ingredient=data['ingredient'], cookingtime=data['cookingtime'], experience=data['experience'])
 
-    result = dict(result=data['result'], name=name)
+      result = dict(result=data['result'], name=name)
 
-    if 'group' in data:
-      result['group'] = data['group']
+      if 'group' in data:
+        result['group'] = data['group']
 
-    recipes[name] = recipe
-    results[name] = result
-    items.append(name)
+      recipes[name] = recipe
+      results[name] = result
+      items.append(name)
+    except Exception as e:
+      print(file.split("\\")[-1].split("_")[-1])
 
   items2 = items[:]
   random.shuffle(items2)
 
   for s,d in zip(items, items2):
     recipes[s].update(results[d])
-    if recipes[s]['type'] == 'smelting':
+    if recipes[s]['type'] == 'minecraft:smelting':
       if isinstance(recipes[s]['result'], dict):
         recipes[s]['result'] = recipes[s]['result']['item']
     else:
@@ -64,3 +69,7 @@ def make_shuffled_datapack():
   output.seek(0)
 
   return output
+
+if __name__ == "__main__":
+  with open("pack.zip", 'wb') as f:
+    f.write(make_shuffled_datapack().read())
